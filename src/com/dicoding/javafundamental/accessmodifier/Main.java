cdf7aea541a9e422c22a0b73d3325e0705753d7d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-09 06:18:55 +0700

    Access Modifier

---
+++ src/com/dicoding/javafundamental/accessmodifier/Main.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental.accessmodifier;
+
+public class Main {
+    public static void main(String[] args) {
+        KelasA kelasA = new KelasA();
+//        System.out.println(kelasA.memberA);
+//        System.out.println(kelasA.functionA());
+        System.out.println(kelasA.functionB());
+    }
+}
