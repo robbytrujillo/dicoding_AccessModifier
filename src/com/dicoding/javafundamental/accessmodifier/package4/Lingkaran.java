d7deeaca55e977813bdb421f6a20d54382ff9f90
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-10 06:16:33 +0700

    Access Modifier | Final Methods

--- src/com/dicoding/javafundamental/accessmodifier/package4/Lingkaran.java
+++ src/com/dicoding/javafundamental/accessmodifier/package4/Lingkaran.java
@@ -2,4 +2,16 @@ package com.dicoding.javafundamental.accessmodifier.package4;
 
 public class Lingkaran {
     static final double PI = 3.14;
+
+    int jari = 7;
+
+    final double getLuas() {
+        return PI * (jari * jari);
+    }
+
+    // Kelas yang dideklarasikan sebagai final maka tidak bisa dijadikan sebagai induk kelas.
+
+//    final class Lingkaran{
+//
+//    }
 }
